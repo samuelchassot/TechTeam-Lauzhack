96e6e10297b3043fe95e81a846b0d2cf46cf3599
samuelchassot <samuel.chassot@epfl.ch>
2018-11-24 19:15:10 +0100

    first wear app working

--- sensorsender/src/main/java/ch/techteam/sensorsender/ClientPaths.java
+++
@@ -1,6 +0,0 @@
-package ch.techteam.sensorsender;
-
-public class ClientPaths {
-    public static final String START_MEASUREMENT = "/start";
-    public static final String STOP_MEASUREMENT = "/stop";
-}
