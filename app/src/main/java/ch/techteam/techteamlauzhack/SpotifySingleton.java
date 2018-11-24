518b3e03fbd80c9baca139f47eca275caf47bc80
Julie Giunta <julie.giunta@epfl.ch>
2018-11-24 17:14:51 +0100

    Use token for spotify

--- app/src/main/java/ch/techteam/techteamlauzhack/SpotifySingleton.java
+++ app/src/main/java/ch/techteam/techteamlauzhack/SpotifySingleton.java
@@ -6,18 +6,25 @@ public class SpotifySingleton {
 
     private static SpotifySingleton instance_;
     private SpotifyAppRemote spotifyAppRemote_;
+    private String accessToken_;
 
     private SpotifySingleton() {
-        spotifyAppRemote_ = null;
+        //spotifyAppRemote_ = null;
+        accessToken_ = null;
     }
 
-    public static void putAppRemote(SpotifyAppRemote spotifyAppRemote) {
+    /*public static void putAppRemote(SpotifyAppRemote spotifyAppRemote) {
         if (instance_ == null) {
             instance_ = new SpotifySingleton();
         }
         instance_.spotifyAppRemote_ = spotifyAppRemote;
 
-    }
+    }*/
+
+
+    /*public SpotifyAppRemote getSpotifyAppRemote_() {
+        return spotifyAppRemote_;
+    }*/
 
     public static SpotifySingleton get() {
         if (instance_ == null) {
@@ -26,8 +33,15 @@ public class SpotifySingleton {
         return instance_;
     }
 
-    public SpotifyAppRemote getSpotifyAppRemote_() {
-        return spotifyAppRemote_;
+    public static void putAccessToken(String accessToken) {
+        if (instance_ == null) {
+            instance_ = new SpotifySingleton();
+        }
+        instance_.accessToken_ = accessToken;
+
     }
 
+        public String getAccessToken() {
+        return accessToken_;
+    }
 }
