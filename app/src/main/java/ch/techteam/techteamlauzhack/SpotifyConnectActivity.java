9183a693b6b7ed4ed24f766a30d64532fe21a82a
Julie Giunta <julie.giunta@epfl.ch>
2018-11-24 15:18:48 +0100

    Connect Spotify

---
+++ app/src/main/java/ch/techteam/techteamlauzhack/SpotifyConnectActivity.java
@@ -0,0 +1,66 @@
+package ch.techteam.techteamlauzhack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.util.Log;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
+public class SpotifyConnectActivity extends AppCompatActivity {
+
+    private static final String CLIENT_ID = "3dd8d94d7032410db6a52b0c51c09c67";
+    private static final String REDIRECT_URI = "ch.techteam.techteamlauzhack://callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spotify_connect);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("SpotifyConnectActivity", "Connected! Yay!");
+
+                        // Now you can start interacting with App Remote
+                        connected();
+
+                    }
+
+                    public void onFailure(Throwable throwable) {
+                        Log.e("SpotifyConnectActivity", throwable.getMessage(), throwable);
+
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
+
+    private void connected() {}
+
+}
